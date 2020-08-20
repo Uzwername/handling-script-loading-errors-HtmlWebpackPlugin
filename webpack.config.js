c47f9a7941fa24e5f9409df9f66643f3cc48a0a4
Igor Bykov <igor.bykov@protonmail.com>
2020-08-20 22:47:16 +0200

    Complete example

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,28 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
 
-
 module.exports = {
     entry: path.resolve(__dirname, 'src/index.js'),
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[hash:5].js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    esModule: false
+                }
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.ejs',
-
+            inject: false
         }),
         new CleanWebpackPlugin()
     ]
